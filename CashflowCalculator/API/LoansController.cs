147643f662bff4aa95d801997fb9c0ab703c4b2b
Peter McNeely <pete.mcneely@gmail.com>
2017-01-27 23:40:51 -0500

    Add basic api support to loans.

---
+++ CashflowCalculator/API/LoansController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+using CashflowCalculator.Models;
+
+namespace CashflowCalculator.API
+{
+    public class LoansController : ApiController
+    {
+        private CashflowCalculatorContext db = new CashflowCalculatorContext();
+
+        public IEnumerable<Loan> GetAllLoans()
+        {
+            return db.Loans.Select(c => c).ToList();
+        }
+
+        public IHttpActionResult GetLoan(int Id)
+        {
+            var loan = db.Loans.FirstOrDefault(l => l.Id == Id);
+            if (loan == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(loan);
+            }
+        }
+    }
+}
